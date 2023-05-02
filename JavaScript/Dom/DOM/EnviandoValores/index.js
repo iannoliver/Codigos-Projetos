9b3ac704020490eb819e34c584b5b816f7b56c0e
IannOli <100435787+iannoliver@users.noreply.github.com>
2023-05-02 12:23:13 -0300

    Add files via upload

---
+++ JavaScript/Dom/DOM/EnviandoValores/index.js
@@ -0,0 +1,12 @@
+function register(element) {
+    const username = element.children.username.value
+    const password = element.children.password.value
+    const passwordConfirmation = element.children.passwordConfirmation.value
+
+    if (password === passwordConfirmation) {
+       alert("Usuário " + username + "registrado") 
+    } else {
+        alert("Senhas não conferem")
+    }
+    //console.log(username, password, passwordConfirmation)
+}
